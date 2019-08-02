41215435d1a3377bc44177cbdd69feffef4a56e7
ahmed <faseeh27@gmail.com>
2019-08-02 19:45:00 +0530

    vars updated

--- src/com/arg/Checkimport.groovy
+++ src/com/arg/Checkimport.groovy
@@ -2,16 +2,17 @@ package src.com.arg;
 
 class Checkimport{
 	def manager
+	def workspace
 	Checkimport(def m,def w){
 	this.manager = m
 	this.workspace = w
 	}
 	
-	checkname(){
+	def checkname(){
 	def name = "Rajesh"
 	def myname = new Import(name)
 	def gotname = myname.getname()
-		if (gotname != NULL && workspace != NULL)
+		if (gotname != null && workspace != null)
 		{
 			println("Name = "+gotname)
 			println("Workspace = "+workspace}
